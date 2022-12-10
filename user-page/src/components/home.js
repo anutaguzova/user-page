5d38dc80b278c3e0b6f8a4026081b2e4fe5896fa
Hanna Huzava <guzovaann@gmail.com>
2022-12-10 10:45:37 +0100

    feat: add multipage

---
+++ user-page/src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+        return (
+            <div>
+               <img className="home" src="https://s3.amazonaws.com/static-asset/op-video-sync/images/production/p-5810396717001-brightcove-when-to-go-to-the-azores-20180726-050333.jpg"></img>
+            </div>
+        );  
+}
+
+
+export default Home;
